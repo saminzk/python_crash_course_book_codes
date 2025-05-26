4e81dae94d9a16b75bf86a2011f912cfd36d54c3
saminzk <saminzkio@gmail.com>
2025-05-26 13:26:02 +0600

    exersice done

---
+++ ch2-variables-data-types/ex_2_4_name_cases.py
@@ -0,0 +1,5 @@
+name = "john smith"
+
+print(name.upper())
+print(name.lower())
+print(name.title())
---
+++ ch2-variables-data-types/ex_2_5_famous_quotes.py
@@ -0,0 +1,7 @@
+famous_quotes = 'A person who never made a mistake never tried anything new.'
+
+famous_person = 'Albert Einstein'
+
+message = f"{famous_person} once said '{famous_quotes}'"
+
+print (message)
